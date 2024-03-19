8e64b9e8e3ad27ff2514d69d26bcae4136c6142d
leandro <leandrocmsantos@gmail.com>
2024-03-19 14:58:35 -0300

    adjust

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-fiap-tech-lc-lr" # example: terraform-tfstates
+    bucket = "terraform-fiap-tech-lc" # example: terraform-tfstates
     key    = "sonarqube/terraform.tfstate"
     region = "us-east-1"  # example: us-east-1 (região escolhida na criação do bucket)
   }
